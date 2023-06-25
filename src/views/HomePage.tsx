2ad996397a70f931aff688edd5fac60f529cc359
unknown <marine.richer41@gmail.com>
2023-06-25 21:52:04 +0200

    change accueil

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -3,61 +3,10 @@ import Nav from './../components/nav.tsx';
 import styles from './../styles/homePage.module.css';
 
 
-//import images
-import dogandcat from './../assets/dogandcat.jpg';
-import dogFood from './../assets/dog/food/food.jpg';
-import ball from './../assets/dog/autre/ball.jpg';
-import arbre from './../assets/cat/autres/arbre.jpg';
-import jouetCat from './../assets/cat/autres/jouet.jpg';
-
-
 export default function HomePage() {
   return (
     <>
       <Nav />
-      <img className={styles.dogandcat} src={dogandcat} />
-      <div className={styles.presentation}>
-        <div className={styles.boxPresntation}>
-          <h2>100% Organique</h2>
-          <div className={styles.textBox}>liposuie wlo oqohcsa dk dkqkqd. dmcnjsdka</div>
-        </div>
-        <div className={styles.boxPresntation}>
-          <h2>100% Francais</h2>
-          <div className={styles.textBox}>liposuie wlo oqohcsa dk dkqkqd. dmcnjsdka</div>
-        </div>
-        <div className={styles.boxPresntation}>
-          <h2>100% Organique</h2>
-          <div className={styles.textBox}>liposuie wlo oqohcsa dk dkqkqd. dmcnjsdka</div>
-        </div>
-        <div className={styles.boxPresntation}>
-          <h2>100% Organique</h2>
-          <div className={styles.textBox}>liposuie wlo oqohcsa dk dkqkqd. dmcnjsdka</div>
-        </div>
-        </div>
-        <div className={styles.titleProducts}>Produits</div>
-        <div className={styles.categoryProduits}>Nos différentes catégories</div>
-        <div className={styles.categories}>
-         
-          <div className={styles.category}>
-            <img className={styles.imgCategories} src={dogFood}></img>
-          </div>
-          <div className={styles.category}>
-          <img className={styles.imgCategories} src={ball}></img>
-          </div>
-          <div className={styles.category}>
-          <img className={styles.imgCategories} src={ball}></img>
-          </div>
-          <div className={styles.category}>
-            <img className={styles.imgCategories} src={dogFood}></img>
-          </div>
-          <div className={styles.category}>
-          <img className={styles.imgCategories} src={dogFood}></img>
-          </div>
-          <div className={styles.category}>
-          <img className={styles.imgCategories} src={jouetCat}></img>
-          </div>
-         
-        </div>
-    </>
-  );
+      </>
+      )
 }
